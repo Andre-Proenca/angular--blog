8f5b0f20f556d470c5c40dbd8761571a94e0e7ce
Andre Proenca <andreproenca@hotmail.com>
2024-02-11 09:28:57 -0400

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photoCover": "https://www.komar.de/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/I/A/IADX8-062.jpg",
+        "title": "NOVO HOMEM DE FERRO EM 3D",
+        "description": "Marvel anuncia novo filme do Homem de Ferro."
+    },
+    {
+        "id":"2",
+        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+        "title": "Nova série anunciada no Disney +",
+        "description": "Descricao"
+    }
+]
